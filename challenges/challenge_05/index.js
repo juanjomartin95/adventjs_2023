7d90e60dc90055df1f060db3dc9da71a8de54078
Juanjo Martin Ramirez <juanjose.martin@adsmurai.com>
2023-12-06 00:44:43 +0100

    feat(challenge_05): add challenge 05 resolved but is not correct

---
+++ challenges/challenge_05/index.js
@@ -0,0 +1,67 @@
+const SLEIGH = 'S';
+const ROAD = '.';
+const OPEN = '*';
+const CLOSE = '|';
+
+const advanceOnePosition = (currentRoad, roadWithoutSleigh) => {
+  const currentRoadSplitted = currentRoad.split('');
+  const sleighPosition = currentRoad.indexOf(SLEIGH);
+  currentRoadSplitted[sleighPosition] = roadWithoutSleigh.at(sleighPosition);
+  currentRoadSplitted[sleighPosition + 1] = SLEIGH
+  return currentRoadSplitted.join('')
+}
+
+const nextActionByRoadElement = {
+  [ROAD]: (currentRoad, roadWithoutSleigh) => advanceOnePosition(currentRoad, roadWithoutSleigh),
+  [OPEN]: (currentRoad, roadWithoutSleigh) => advanceOnePosition(currentRoad, roadWithoutSleigh),
+  [CLOSE]: (currentRoad) => currentRoad,
+};
+
+const executeNextAction = (currentRoad, roadWithoutSleigh) => {
+  const nextElementPosition = currentRoad.indexOf(SLEIGH) + 1;
+  const nextElement = currentRoad.at(nextElementPosition);
+  return nextElementPosition === -1
+      ? currentRoad
+      : nextActionByRoadElement[nextElement](currentRoad, roadWithoutSleigh);
+};
+
+const openBarriers = (currentRoad) => {
+  return currentRoad.replaceAll(CLOSE, OPEN);
+};
+
+function cyberReindeer(road, time) {
+  let roadWithoutSleigh = road.replaceAll(SLEIGH, ROAD)
+  if (road.length === 0 || time === 0) return [];
+
+  let roadTimeline = [];
+
+  let currentRoad = road;
+  for (let i = 0; i < time || i < road.length; i++) {
+    currentRoad = executeNextAction(currentRoad, roadWithoutSleigh);
+    roadTimeline.push(currentRoad);
+    if (i === 4) {
+      currentRoad = openBarriers(currentRoad);
+      roadWithoutSleigh = openBarriers(roadWithoutSleigh)
+    }
+  }
+  return roadTimeline;
+}
+
+let road = 'S..|...|..';
+const time = 10; // unidades de tiempo
+const result = cyberReindeer(road, time);
+/* -> result:
+[
+  'S..|...|..', // estado inicial
+  '.S.|...|..', // avanza el trineo la carretera
+  '..S|...|..', // avanza el trineo la carretera
+  '..S|...|..', // el trineo para en la barrera
+  '..S|...|..', // el trineo para en la barrera
+  '...S...*..', // se abre la barrera, el trineo avanza
+  '...*S..*..', // avanza el trineo la carretera
+  '...*.S.*..', // avanza el trineo la carretera
+  '...*..S*..', // avanza el trineo la carretera
+  '...*...S..', // avanza por la barrera abierta
+]
+*/
+console.log(result);
